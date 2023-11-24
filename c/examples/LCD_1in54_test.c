4b8bbc61376903e8e83b003ca8d724f01781c225
Felix <felix@schoenherrs.com>
2023-11-24 12:19:21 +0100

    test

--- c/examples/LCD_1in54_test.c
+++ c/examples/LCD_1in54_test.c
@@ -6,6 +6,7 @@
 #include <stdio.h>  //printf()
 #include <stdlib.h> //exit()
 #include <signal.h> //signal()
+#include <dirent.h>
 
 void LCD_1IN54_test(void)
 {
@@ -23,7 +24,8 @@ void LCD_1IN54_test(void)
     printf("1.54inch LCD demo...\r\n");
     LCD_1IN54_Init(HORIZONTAL);
     LCD_1IN54_Clear(WHITE);
-    // LCD_SetBacklight(1023);
+    LCD_SetBacklight(1023);
+
     UDOUBLE Imagesize = LCD_1IN54_HEIGHT * LCD_1IN54_WIDTH * 2;
     UWORD *BlackImage;
     if ((BlackImage = (UWORD *)malloc(Imagesize)) == NULL)
@@ -34,19 +36,42 @@ void LCD_1IN54_test(void)
     // /*1.Create a new image cache named IMAGE_RGB and fill it with white*/
     Paint_NewImage(BlackImage, LCD_1IN54_WIDTH, LCD_1IN54_HEIGHT, 0, WHITE, 16);
     Paint_Clear(WHITE);
-    Paint_SetRotate(ROTATE_90);
+    // Paint_SetRotate(ROTATE_90);
 
     // /* show bmp */
     printf("show bmp\r\n");
 
-    for (int i = 1; i <= 2; i++)
-    {                      // Assuming you have test1.bmp and test2.bmp
-        char filename[20]; // Adjust the size according to your filename length
-        snprintf(filename, sizeof(filename), "./pic/test%d.bmp", i);
+    DIR *dir;
+    struct dirent *ent;
+    char folder_path[] = "./pic/";
 
-        GUI_ReadBmp(filename);
-        LCD_1IN54_Display(BlackImage);
-        DEV_Delay_ms(100000);
+    if ((dir = opendir(folder_path)) != NULL)
+    {
+        int i = 0;
+        while ((ent = readdir(dir)) != NULL)
+        {
+            if (ent->d_type == DT_REG && strstr(ent->d_name, ".bmp") != NULL && strstr(ent->d_name, "test") != NULL)
+            {
+                char filename[100];
+                snprintf(filename, sizeof(filename), "%s%s", folder_path, ent->d_name);
+
+                GUI_ReadBmp(filename);
+                LCD_1IN54_Display(BlackImage);
+                DEV_Delay_ms(5000);
+                i++;
+            }
+        }
+        closedir(dir);
+
+        if (i == 0)
+        {
+            printf("No BMP files found in %s\r\n", folder_path);
+        }
+    }
+    else
+    {
+        perror("Unable to open directory");
+        exit(EXIT_FAILURE);
     }
 
     /* Module Exit */
